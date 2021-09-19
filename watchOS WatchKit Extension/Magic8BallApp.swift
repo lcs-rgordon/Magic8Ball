303873e353a1fb8a56259506577c37e78ec16062
Russell Gordon <rgordon@lcs.on.ca>
2021-09-19 13:29:19 -0400

    Basic watchOS version of the app added.

---
+++ watchOS WatchKit Extension/Magic8BallApp.swift
@@ -0,0 +1,19 @@
+//
+//  Magic8BallApp.swift
+//  watchOS WatchKit Extension
+//
+//  Created by Russell Gordon on 2021-09-19.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
